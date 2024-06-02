6c6dfcb9c44854cbcb9846a866b7beda2192dc86
jbm950 <jmilam343@gmail.com>
2024-06-02 13:47:22 -0400

    feat(kanban): collect task state during import
    This commit adds grabbing the state of the task while importing the
    data. It assumes the state can be determined by the name of the first
    subfolder under Productivity. Until a time where all the tasks are moved
    to a flattened structure, this will have to do for now.
    
    Refs:

--- kanban/kanban_main.py
+++ kanban/kanban_main.py
@@ -17,6 +17,10 @@ from prompt_toolkit.widgets import Frame
 class Task:
     def __init__(self, file_path, projects=None):
         self.file_path = file_path
+        # This gets the name of the first folder after "Productivity" and
+        # assume that it represents the state of the task (ex. Backlog,
+        # Completed, etc.).
+        self.state = file_path.relative_to(NOTES_DIR).parents[-3].name
         self.projects: list = projects
 
 class TaskList(list):
