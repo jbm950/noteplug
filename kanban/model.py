7f65a14b270db22797530f56fd9d23321d4305e2
jbm950 <jmilam343@gmail.com>
2024-06-02 15:20:25 -0400

    feat(kanban model): add method to collect tasks for a given project
    This commit extends the task list functionality by giving it a method to
    filter for a specified project. It structures the return to be sorted by
    task status. Maybe a future version would just make a new version of
    TaskList?
    
    Refs:

--- kanban/model.py
+++ kanban/model.py
@@ -3,11 +3,12 @@ from constants import PROD_DIR
 
 class Task:
     def __init__(self, file_path, projects=None):
+        self.name = file_path.name
         self.file_path = file_path
         # This gets the name of the first folder after "Productivity" and
         # assume that it represents the state of the task (ex. Backlog,
         # Completed, etc.).
-        self.state = file_path.relative_to(PROD_DIR).parents[-2].name
+        self.status = file_path.relative_to(PROD_DIR).parents[-2].name
         self.projects: list = projects
 
 
@@ -22,3 +23,13 @@ class TaskList(list):
                     projects_set.add(project)
 
         return sorted(list(projects_set))
+
+    def task_status_for_project(self, project):
+        result = {"Active": [], "Archived": [], "Backlog": [], "Completed": []}
+        for task in self:
+            if task.projects is not None and project in task.projects:
+                status = task.status if task.status != "Projects" else "Active"
+                result[status].append(task)
+
+        return result
+
