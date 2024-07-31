50a27616e27d29a8921274af8f3dc1ee2967cc96
jbm950 <jmilam343@gmail.com>
2024-07-30 20:40:22 -0400

    fix(kanban): specify file encoding to account for emojis
    The code that read in all of the markdown files was throwing errors
    since the files have emojis and that doesn't line up with the default
    encoding. This commit makes the encoding as "utf8" explicit so that no
    errors are raised while reading the files.
    
    Refs:

--- kanban/load_tasks.py
+++ kanban/load_tasks.py
@@ -7,7 +7,7 @@ from model import Task, TaskList
 def load_tasks():
     task_list = TaskList()
     for markdown_file_path in PROD_DIR.rglob("*.md"):
-        with open(markdown_file_path) as markdown_file:
+        with open(markdown_file_path, encoding="utf8") as markdown_file:
             first_line = markdown_file.readline()
             if first_line != "---\n":
                 task_list.append(Task(markdown_file_path))
